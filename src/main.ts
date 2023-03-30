3261dddbff4acf38f2057341b20d53638ff4efd7
Abhishek kr <abhimait1909@gmail.com>
2023-03-30 22:21:27 +0530

    output final markdown as a result of tableRows data
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
-import { CypressResult, Result } from './typeDefinition'
+import { CypressResult, Result } from './cypressTest'
+import { TableSummary } from './tableSummary'
 
 async function run(): Promise<void> {
   try {
@@ -8,21 +8,24 @@ async function run(): Promise<void> {
     core.debug(`Waiting ${jsonInput} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.info(`Json Input ${jsonInput}`);
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(jsonInput, 10))
-    core.debug(new Date().toTimeString())
+    const finalJsonResponse = getTableOutputAsJson(JSON.stringify(jsonInput));
+    finalJsonResponse.map(console.log);
 
-    core.setOutput('time', new Date().toTimeString())
+    const mardownresult = convertJsonToMardownTable(finalJsonResponse);
+    console.log(mardownresult);
+    core.info(mardownresult);
+
+    core.setOutput('mardownResult', mardownresult)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-function getTableOutputAsJson(jsonInput: string): any {
+function getTableOutputAsJson(jsonInput: string): TableSummary[] {
   const cypressJsonResult: CypressResult = JSON.parse(jsonInput);
   const testResult: Result[] = cypressJsonResult.results;
 
-  testResult.map((result: Result) => {
+  return testResult.map((result: Result): TableSummary => {
     return {
       title: result.title,
       total: result.suites.reduce((prev, curr) => {
@@ -39,11 +42,25 @@ function getTableOutputAsJson(jsonInput: string): any {
       }, 0),
       pending: result.suites.reduce((prev, curr) => {
         return prev + curr.pending.length
+      }, 0),
+      success: result.suites.reduce((prev, curr) => {
+        return prev + curr.passes.length
       }, 0)
     }
   })
-
-  return {}
 }
+
+function convertJsonToMardownTable(cypressJson: TableSummary[]) {
+  const tableHeaderMd = convertRowToMd(tableHeader);
+
+  const tableDataRows = cypressJson.map((sum) => convertRowToMd(Object.values(sum)));
+
+  return tableHeaderMd + tableHeader.map(mp => "--| ") + "\n" + tableDataRows;
+}
+
+const convertRowToMd = (columns: (string | Number)[]) => `|${columns.map((col: (string | Number)) => col + "|")}\n`
+
+
+const tableHeader = ["Title", "Skipped", "Pending", "Failures", "", "Total"]
 
 run()
---
+++ src/tableSummary.d.ts
@@ -0,0 +1,9 @@
+export interface TableSummary {
+  title: String;
+  total: Number;
+  skipped: Number;
+  duration: Number;
+  failures: Number;
+  pending: Number;
+  success: Number;
+}
