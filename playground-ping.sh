79d161ece031bb7cd7ecde682aaef383f415b032
Abhishek kr <abhimait1909@gmail.com>
2023-05-24 01:06:29 +0530

    extract actice cluster to look for
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- playground-ping.sh
+++ playground-ping.sh
@@ -1,5 +1,8 @@
-PLAYGROUND_PING_URL=https://playground.meshery.io/api/system/version
+# Pings the Playground active server
+# returns true if the playground responds wit 200
+# else returns false
 
+PLAYGROUND_PING_URL=https://playground.meshery.io/api/system/version
 export V=($(curl --silent  $PLAYGROUND_PING_URL  -w "%{http_code}" | grep 200))
 
 if [ ! -z "$V" ] && [ $V == 200 ]
