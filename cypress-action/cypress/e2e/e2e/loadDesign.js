21000a721972de9b13cabaf8bc49a9be6ee2856c
Abhishek kr <abhimait1909@gmail.com>
2023-05-25 02:33:51 +0530

    add cypress and puppeteer github actions
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

---
+++ cypress-action/cypress/e2e/e2e/loadDesign.js
@@ -0,0 +1,13 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+/// <reference types="../../support" /> 
+
+import { beforeEachCallback } from "../../support/helpers";
+
+describe("Canvas Double Click Test", () => {
+  beforeEach(beforeEachCallback)
+
+  it("load a design/application with ID", () => {
+    
+  })
+})
--- node-action/src/table-summary-definition.ts
+++
@@ -1,9 +0,0 @@
-export interface TableSummary {
-  title: String
-  total?: Number
-  skipped: Number
-  duration: String
-  failures: Number
-  pending: Number
-  success: Number
-}
