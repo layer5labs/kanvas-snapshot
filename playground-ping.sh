065993dc57a3513366164bf3b591fdd98614946e
Abhishek kr <abhimait1909@gmail.com>
2023-06-01 02:05:24 +0530

    test in the absense of playground
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- playground-ping.sh
+++ playground-ping.sh
@@ -2,12 +2,14 @@
 # returns true if the playground responds wit 200
 # else returns false
 
-PLAYGROUND_PING_URL=https://playground.meshery.io/api/system/version
-export V=($(curl --silent  $PLAYGROUND_PING_URL  -w "%{http_code}" | grep 200))
+# PLAYGROUND_PING_URL=https://playground.meshery.io/api/system/version
+# export V=($(curl --silent  $PLAYGROUND_PING_URL  -w "%{http_code}" | grep 200))
 
-if [ ! -z "$V" ] && [ $V == 200 ]
-then
-  echo true
-else 
-  echo false
-fi
+# if [ ! -z "$V" ] && [ $V == 200 ]
+# then
+#   echo true
+# else 
+#   echo false
+# fi
+
+echo false
