6725fa4c6d8aa9d95af5470c37886224960a0fd7
Abhishek kr <abhimait1909@gmail.com>
2023-06-01 02:10:04 +0530

    test

--- playground-ping.sh
+++ playground-ping.sh
@@ -2,14 +2,12 @@
 # returns true if the playground responds wit 200
 # else returns false
 
-# PLAYGROUND_PING_URL=https://playground.meshery.io/api/system/version
-# export V=($(curl --silent  $PLAYGROUND_PING_URL  -w "%{http_code}" | grep 200))
+PLAYGROUND_PING_URL=https://playground.meshery.io/api/system/version
+export V=($(curl --silent  $PLAYGROUND_PING_URL  -w "%{http_code}" | grep 200))
 
-# if [ ! -z "$V" ] && [ $V == 200 ]
-# then
-#   echo true
-# else 
-#   echo false
-# fi
-
-echo false
+if [ ! -z "$V" ] && [ $V == 200 ]
+then
+  echo true
+else 
+  echo false
+fi
