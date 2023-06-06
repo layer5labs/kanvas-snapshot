1231e256f7a21443cb85cc89fb2fdcaedbef2f59
Abhishek kr <abhimait1909@gmail.com>
2023-06-07 04:25:38 +0530

    debug: check appfile
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- uploadApplication.sh
+++ uploadApplication.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
 # ENV Variables expected
 # MESHERY_SERVER_BASE_URL
 # UPLOAD_TYPE: can be "Kubernetes Manifest" | "Helm Chart" | "Docker Compose"
 # PROVIDER_TOKEN: MESHERY provider token
 
-# get Meshery application file as escaped yaml str
-MESHERY_APPLICATION_FILE=$(awk '{printf "%s\\n", $0}' __intermediate_file.yml)
-echo "App file:" $MESHERY_APPLICATION_FILE
+# # get Meshery application file as escaped yaml str
+MESHERY_APPLICATION_FILE=$(awk '{ gsub(/"/, "\\\"", $0); printf "%s\\n", $0}' __intermediate_file.yml)
 
-# convert to uri-encoded str
+# # convert to uri-encoded str
 UPLOAD_TYPE=$(printf %s "$UPLOAD_TYPE" | jq -sRr @uri)
 
 curl "$MESHERY_SERVER_BASE_URL/api/application/$UPLOAD_TYPE" \
@@ -16,5 +16,5 @@ curl "$MESHERY_SERVER_BASE_URL/api/application/$UPLOAD_TYPE" \
   -H 'Content-Type: text/plain;charset=UTF-8' \
   -H "Cookie: meshery-provider=Meshery; token=$PROVIDER_TOKEN;" \
   --data-raw "{\"save\":false, \"application_data\": {\"application_file\":\"$MESHERY_APPLICATION_FILE\"}}" \
-  --compressed --trace-ascii -
+  --compressed | jq "."
 
