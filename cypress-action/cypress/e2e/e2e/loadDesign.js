1a2a090290a38cc31642fc659499cb29a7149c8c
Abhishek kr <abhimait1909@gmail.com>
2023-06-07 20:48:40 +0530

    relace quotes in string
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- cypress-action/cypress/e2e/e2e/loadDesign.js
+++ cypress-action/cypress/e2e/e2e/loadDesign.js
@@ -6,7 +6,7 @@ import { TIME } from "../../support/constants";
 import { beforeEachCallbackForCustomUrl } from "../../support/helpers";
 
 describe("Infra Shot Automated Runner", () => {
-  beforeEach(()=> beforeEachCallbackForCustomUrl(`/extension/meshmap?application=${Cypress.env("applicationId")}`))
+  beforeEach(()=> beforeEachCallbackForCustomUrl(`/extension/meshmap?application=${Cypress.env("applicationId").replace(/['"]+/g, '')}`))
 
   it("load a design/application with ID", () => {
     cy.wait(TIME.XXXLARGE)
