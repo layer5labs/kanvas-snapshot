cad03b5276bac3a171642bb59cee816892fce44f
Abhishek kr <abhimait1909@gmail.com>
2023-08-01 20:13:56 +0530

    Fix: snapshotting service of MeshMap canvas
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- cypress-action/cypress/support/helpers.js
+++ cypress-action/cypress/support/helpers.js
@@ -30,3 +30,38 @@ export const beforeEachCallbackForCustomUrl = (customPath) => {
   cy.visit(customPath);
   cy.wait(waitFor(extension.alias), { timeout: 60_000 });
 }
+
+export const saveGraph = (cy) => {
+  let image = cy.png();
+  let lnk = document.createElement("a"),
+    date = new Date(),
+    e;
+  lnk.href = image;
+  lnk.download =
+    "MeshMap-" + date.toDateString() + "/" + date.toLocaleTimeString() + ".png";
+
+  if (document.createEvent) {
+    e = document.createEvent("MouseEvents");
+    e.initMouseEvent(
+      "click",
+      true,
+      true,
+      window,
+      0,
+      0,
+      0,
+      0,
+      0,
+      false,
+      false,
+      false,
+      false,
+      0,
+      null
+    );
+
+    lnk.dispatchEvent(e);
+  } else if (lnk.fireEvent) {
+    lnk.fireEvent("onclick");
+  }
+};
