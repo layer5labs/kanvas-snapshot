dd890ba4a575a402ae10422f7707367312005586
Pranav Singh <pranavsingh02@hotmail.com>
2024-01-21 01:24:41 +0530

    add support to convert design into unsigned 8-bit array
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

---
+++ normalize-configuration-file/index.js
@@ -0,0 +1,12 @@
+// TODO: import this from Sistent
+const getUnit8ArrayForDesign = (design) => {
+  const uint8Array = Uint8Array.from(design, (char) => char.charCodeAt(0));
+
+  return Array.from(uint8Array);
+};
+
+
+const pattern_file = getUnit8ArrayForDesign(process.env.pattern_file);
+console.dir(pattern_file, {'maxArrayLength': null}); // necessary to override the default maxArrayLength of 100
+
+
