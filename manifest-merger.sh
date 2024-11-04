3f346e7642f63c55367e7396d2d6b22191b4f8fe
jerensl <54782057+jerensl@users.noreply.github.com>
2024-11-04 17:44:12 +0800

    fix: fix single file with no newline will fail the action
    Signed-off-by: jerensl <54782057+jerensl@users.noreply.github.com>

--- manifest-merger.sh
+++ manifest-merger.sh
@@ -1,14 +1,21 @@
 touch __intermediate_file.yml
 echo $FILE_PATH
+
+append_with_newline() {
+  file=$1
+  cat "$file" >> __intermediate_file.yml
+  if [ -n "$(tail -c 1 "$file")" ]; then
+    echo "" >> __intermediate_file.yml
+  fi
+  echo "---" >> __intermediate_file.yml
+}
+
 if [[ -d $FILE_PATH ]]; then
-  for file in $FILE_PATH/*; do # FILE_PATH is the environment variable
-    echo $file
-    if [[ $file == *yaml ]] ||  [[ $file == *yml ]]
-    then 
-      cat $file >> __intermediate_file.yml
-      echo "---" >> __intermediate_file.yml
+  for file in "$FILE_PATH"/*; do
+    if [[ $file == *.yaml || $file == *.yml ]]; then
+      append_with_newline "$file"
     fi
-  done;
+  done
 else
- cat $FILE_PATH >> __intermediate_file.yml
+  append_with_newline "$FILE_PATH"
 fi
