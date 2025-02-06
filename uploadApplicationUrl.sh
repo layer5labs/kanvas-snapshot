88c8ddbd7b291e9b95abe2ca73fcba4ecb589200
aabidsofi19 <mailtoaabid01@gmail.com>
2025-02-07 02:15:39 +0530

    update import endpoints
    Signed-off-by: aabidsofi19 <mailtoaabid01@gmail.com>

--- uploadApplicationUrl.sh
+++ uploadApplicationUrl.sh
@@ -6,12 +6,12 @@
 # UPLOAD_URL: Pattern's url upload link
 
 # # convert to uri-encoded str
-UPLOAD_TYPE=$(printf %s "$UPLOAD_TYPE" | jq -sRr @uri)
+# UPLOAD_TYPE=$(printf %s "$UPLOAD_TYPE" | jq -sRr @uri)
 
-curl "$MESHERY_SERVER_BASE_URL/api/pattern/$UPLOAD_TYPE" \
+curl "$MESHERY_SERVER_BASE_URL/api/pattern/import" \
   -H 'Accept: */*' \
   -H 'Connection: close' \
   -H 'Content-Type: text/plain;charset=UTF-8' \
   -H "Cookie: meshery-provider=Meshery; token=$PROVIDER_TOKEN;" \
-  --data-raw "{\"save\":true, \"url\": \"$UPLOAD_URL\"}" \
+  --data-raw "{\"url\": \"$UPLOAD_URL\"}" \
   --compressed | jq ".[0].id"
