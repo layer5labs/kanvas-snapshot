032e996fb4cf3b66ddeec80ab305db08a72a3b8d
Eti Ijeoma <ijayeti@gmail.com>
2025-03-17 15:22:18 +0100

    write bash scrip to send snapshot to provided email
    Signed-off-by: Eti Ijeoma <ijayeti@gmail.com>

--- sendSnapshotEmail.sh
+++ sendSnapshotEmail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ENV Variables expected:
-# MESHERY_CLOUD_API_URL: Base URL of the Layer5 Cloud API (e.g., "https://cloud.layer5.io")
-# PROVIDER_TOKEN: Meshery provider authentication token
+# MESHERY_CLOUD_API_URL: Base URL of the Layer5 Cloud API
+# MESHERY_TOKEN: Meshery Cloud API authentication token
 # EMAIL: Recipient email address
 # IMAGE_URI: The URI of the snapshot image
 
@@ -10,13 +10,9 @@ response=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$MESHERY_CLOUD_API_UR
   -H 'Accept: application/json' \
   -H 'Connection: close' \
   -H 'Content-Type: application/json' \
-  -H "Cookie: provider_token=$PROVIDER_TOKEN" \
-  --data-raw "{
-    \"to\": \"$EMAIL\",
-    \"subject\": \"Kanvas Design Snapshot\",
-    \"imageURI\": \"$IMAGE_URI\"
-  }" \
-  --compressed)
+  -H "Cookie: provider_token=$MESHERY_TOKEN" \
+  --data "{ \"subject\" : \"Kanvas Design Snapshot\", \"to\":\"$EMAIL\", \"image_uri\" :\"$IMAGE_URI\" }")
+
 
 # Check HTTP status code
 if [ "$response" -eq 200 ]; then
